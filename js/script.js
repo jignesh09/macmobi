44a554bbe7500001392d1fe82d149fe8351d26ca
Jignesh <nirav.changela@phpprogrammers.com>
2013-12-07 10:47:12 +0530

    Childbrowser

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var serviceURL = 'http://184.107.213.34/~projects/demo_projects/mobile/macmobi/webservice.php?';
 $( document ).ready(function() {
-    $("#openstoremenu").click(function(){
+    $(".openstoremenu").click(function(){
         window.plugins.childBrowser.showWebPage('https://store.marantecamerica.com/index.php?main_page=index&amp;cPath=1',
                                         { showLocationBar: true });
     });
